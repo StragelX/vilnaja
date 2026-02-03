f125c404fcdfa7276134cdf8adf0c9d8b378f2c2
anton shulgin <stragelx@gmail.com>
2026-02-03 16:55:37 +0100

    aa

---
+++ assets/index-x_Fujs__.js
@@ -0,0 +1,112 @@
+(function polyfill() {
+  const relList = document.createElement("link").relList;
+  if (relList && relList.supports && relList.supports("modulepreload")) {
+    return;
+  }
+  for (const link of document.querySelectorAll('link[rel="modulepreload"]')) {
+    processPreload(link);
+  }
+  new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      if (mutation.type !== "childList") {
+        continue;
+      }
+      for (const node of mutation.addedNodes) {
+        if (node.tagName === "LINK" && node.rel === "modulepreload")
+          processPreload(node);
+      }
+    }
+  }).observe(document, { childList: true, subtree: true });
+  function getFetchOpts(link) {
+    const fetchOpts = {};
+    if (link.integrity)
+      fetchOpts.integrity = link.integrity;
+    if (link.referrerPolicy)
+      fetchOpts.referrerPolicy = link.referrerPolicy;
+    if (link.crossOrigin === "use-credentials")
+      fetchOpts.credentials = "include";
+    else if (link.crossOrigin === "anonymous")
+      fetchOpts.credentials = "omit";
+    else
+      fetchOpts.credentials = "same-origin";
+    return fetchOpts;
+  }
+  function processPreload(link) {
+    if (link.ep)
+      return;
+    link.ep = true;
+    const fetchOpts = getFetchOpts(link);
+    fetch(link.href, fetchOpts);
+  }
+})();
+let bodyLockStatus = true;
+let bodyLockToggle = (delay = 500) => {
+  if (document.documentElement.classList.contains("lock")) {
+    bodyUnlock(delay);
+  } else {
+    bodyLock(delay);
+  }
+};
+let bodyUnlock = (delay = 500) => {
+  let body = document.querySelector("body");
+  if (bodyLockStatus) {
+    let lock_padding = document.querySelectorAll("[data-lp]");
+    setTimeout(() => {
+      for (let index = 0; index < lock_padding.length; index++) {
+        const el = lock_padding[index];
+        el.style.paddingRight = "0px";
+      }
+      body.style.paddingRight = "0px";
+      document.documentElement.classList.remove("lock");
+    }, delay);
+    bodyLockStatus = false;
+    setTimeout(function() {
+      bodyLockStatus = true;
+    }, delay);
+  }
+};
+let bodyLock = (delay = 500) => {
+  let body = document.querySelector("body");
+  if (bodyLockStatus) {
+    let lock_padding = document.querySelectorAll("[data-lp]");
+    for (let index = 0; index < lock_padding.length; index++) {
+      const el = lock_padding[index];
+      el.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+    }
+    body.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+    document.documentElement.classList.add("lock");
+    bodyLockStatus = false;
+    setTimeout(function() {
+      bodyLockStatus = true;
+    }, delay);
+  }
+};
+function menuInit() {
+  if (document.querySelector(".icon-menu")) {
+    document.addEventListener("click", function(e) {
+      if (bodyLockStatus && e.target.closest(".icon-menu")) {
+        bodyLockToggle();
+        document.documentElement.classList.toggle("menu-open");
+      }
+      const targetLink = e.target.closest(".menu__link");
+      if (document.documentElement.classList.contains("menu-open") && targetLink) {
+        document.documentElement.style.scrollBehavior = "auto";
+        menuClose();
+        const targetId = targetLink.getAttribute("href");
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: "auto" });
+        }
+        setTimeout(() => {
+          document.documentElement.style.scrollBehavior = "smooth";
+        }, 10);
+      }
+    });
+  }
+}
+function menuClose() {
+  bodyUnlock();
+  document.documentElement.classList.remove("menu-open");
+}
+window["FLS"] = true;
+menuInit();
